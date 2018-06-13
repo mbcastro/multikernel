c592a02263b63839bef80092eccd1c330c801200
Jordan <jordan73@live.fr>
2018-06-13 11:04:47 -0300

    introducing portal_t

--- include/nanvix/arch/mppa.h
+++ include/nanvix/arch/mppa.h
@@ -1,18 +1,18 @@
 /*
  * Copyright(C) 2011-2018 Pedro H. Penna <pedrohenriquepenna@gmail.com>
- * 
+ *
  * This file is part of Nanvix.
- * 
+ *
  * Nanvix is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Nanvix is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -77,5 +77,19 @@
 	 * @brief Size (in bytes) of a mailbox message.
 	 */
 	#define HAL_MAILBOX_MSG_SIZE 64
+
+/*===========================================================================*
+ * Portal                                                                    *
+ *===========================================================================*/
+
+	/**
+	 * @brief HAL portal.
+	 */
+	typedef struct
+	{
+		int portal_fd;  /* Portal NoC connector.     */
+		int sync_fd;    /* Sync NoC connector.       */
+		int remote;     /* NoC node ID of the remote */
+	} *portal_t;
 
 #endif /* NANVIX_ARCH_MPPA256 */
