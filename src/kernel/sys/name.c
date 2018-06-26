9a3fed2ee6bdb35bb844801c9c112fe4e1fcb1f1
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2018-06-26 09:32:27 -0300

    Fixing Build Errors

---
+++ src/kernel/sys/name.c
@@ -0,0 +1,100 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2011-2018 Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.  THE SOFTWARE IS PROVIDED
+ * "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <pthread.h>
+
+#include <nanvix/syscalls.h>
+
+/* Forward definitions. */
+extern pthread_mutex_t kernel_lock;
+
+/*============================================================================*
+ * name_lookup()                                                              *
+ *============================================================================*/
+
+/**
+ * @brief Converts a name into a NoC node ID.
+ *
+ * @param name Target name.
+ *
+ * @returns Upon successful completion the NoC node ID whose name is @p
+ * name is returned. Upon failure, a negative error code is returned
+ * instead.
+ */
+int name_lookup(char *name)
+{
+	int ret;
+
+	pthread_mutex_lock(&kernel_lock);
+		ret = sys_name_lookup(name);
+	pthread_mutex_unlock(&kernel_lock);
+
+	return (ret);
+}
+
+/*============================================================================*
+ * name_link()                                                                *
+ *============================================================================*/
+
+/**
+ * @brief link a process name.
+ *
+ * @param nodeid NoC node ID of the process to link.
+ * @param name   Name of the process to link.
+ *
+ * @returns Upon successful completion 0 is returned.
+ * Upon failure, a negative error code is returned instead.
+ */
+int name_link(int nodeid, const char *name)
+{
+	int ret;
+
+	pthread_mutex_lock(&kernel_lock);
+		ret = sys_name_link(nodeid, name);
+	pthread_mutex_unlock(&kernel_lock);
+
+	return (ret);
+}
+
+/*============================================================================*
+ * name_unlink()                                                              *
+ *============================================================================*/
+
+/**
+ * @brief Unlink a process name.
+ *
+ * @param name	    Name of the process to unlink.
+ *
+ * @returns Upon successful completion 0 is returned.
+ * Upon failure, a negative error code is returned instead.
+ */
+int name_unlink(const char *name)
+{
+	int ret;
+
+	pthread_mutex_lock(&kernel_lock);
+		ret = sys_name_unlink(name);
+	pthread_mutex_unlock(&kernel_lock);
+
+	return (ret);
+}
+
