45d6c2ef9b0d059e678471fa5655eb42dc816329
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2018-07-16 08:53:23 -0300

    Updating HAL Diagrams

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -1,7 +1,9 @@
+
+export PLATUML="java -jar /opt/plantuml/plantuml.jar"
 mkdir -p doc/class-diagrams
 
- java -jar /opt/plantuml/plantuml.jar doc/uml/hal.uml && mv  doc/uml/hal.png doc/class-diagrams/microkernel.hal.png
- java -jar /opt/plantuml/plantuml.jar doc/uml/mppa256.uml && mv doc/uml/mppa256.png doc/class-diagrams/microkernel.arch.mppa256.png
- java -jar /opt/plantuml/plantuml.jar doc/uml/ipc.uml && mv doc/uml/ipc.png doc/class-diagrams/microkernel.ipc.png
- java -jar /opt/plantuml/plantuml.jar doc/uml/nameserver.uml && mv doc/uml/nameserver.png doc/class-diagrams/servers.name.png
- java -jar /opt/plantuml/plantuml.jar doc/uml/microkernel.uml && mv doc/uml/microkernel.png doc/class-diagrams/microkernel.png
+$PLATUML doc/uml/hal.uml &&      mv  doc/uml/hal.png doc/class-diagrams/microkernel.hal.png
+$PLATUML doc/uml/mppa256.uml     && mv doc/uml/mppa256.png doc/class-diagrams/microkernel.arch.mppa256.png
+$PLATUML doc/uml/ipc.uml         && mv doc/uml/ipc.png doc/class-diagrams/microkernel.ipc.png
+$PLATUML doc/uml/nameserver.uml  && mv doc/uml/nameserver.png doc/class-diagrams/servers.name.png
+$PLATUML doc/uml/microkernel.uml && mv doc/uml/microkernel.png doc/class-diagrams/microkernel.png
