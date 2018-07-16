3088f46574d33b1f28bcb9a16d995ffef01554a1
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2018-07-16 15:19:35 -0300

    Adding Overview Diagram

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -2,8 +2,8 @@
 export PLATUML="java -jar /opt/plantuml/plantuml.jar"
 mkdir -p doc/class-diagrams
 
-$PLATUML doc/uml/hal.uml &&      mv  doc/uml/hal.png doc/class-diagrams/microkernel.hal.png
-$PLATUML doc/uml/mppa256.uml     && mv doc/uml/mppa256.png doc/class-diagrams/microkernel.arch.mppa256.png
-$PLATUML doc/uml/ipc.uml         && mv doc/uml/ipc.png doc/class-diagrams/microkernel.ipc.png
-$PLATUML doc/uml/nameserver.uml  && mv doc/uml/nameserver.png doc/class-diagrams/servers.name.png
-$PLATUML doc/uml/microkernel.uml && mv doc/uml/microkernel.png doc/class-diagrams/microkernel.png
+#$PLATUML doc/uml/hal.uml &&      mv  doc/uml/hal.png doc/class-diagrams/microkernel.hal.png
+#$PLATUML doc/uml/mppa256.uml     && mv doc/uml/mppa256.png doc/class-diagrams/microkernel.arch.mppa256.png
+#$PLATUML doc/uml/ipc.uml         && mv doc/uml/ipc.png doc/class-diagrams/microkernel.ipc.png
+#$PLATUML doc/uml/nameserver.uml  && mv doc/uml/nameserver.png doc/class-diagrams/servers.name.png
+$PLATUML doc/uml/nanvix.uml && mv doc/uml/nanvix.png doc/class-diagrams/nanvix.png
