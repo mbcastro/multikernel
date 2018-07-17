183a0c59b003c9282ba4e6b4864fa9443030ab9b
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2018-07-17 16:03:29 -0300

    Updating Kernel Class Diagram

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -4,6 +4,7 @@ mkdir -p doc/class-diagrams
 
 $PLATUML doc/uml/kernel-hal.uml         && mv doc/uml/kernel-hal.png doc/class-diagrams/kernel-hal.png
 $PLATUML doc/uml/kernel-hal-mppa256.uml && mv doc/uml/kernel-hal-mppa256.png doc/class-diagrams/kernel-hal-mppa256.png
+$PLATUML doc/uml/kernel.uml             && mv doc/uml/kernel.png doc/class-diagrams/kernel.png
 #$PLATUML doc/uml/ipc.uml         && mv doc/uml/ipc.png doc/class-diagrams/microkernel.ipc.png
 #$PLATUML doc/uml/nameserver.uml  && mv doc/uml/nameserver.png doc/class-diagrams/servers.name.png
 #$PLATUML doc/uml/nanvix.uml && mv doc/uml/nanvix.png doc/class-diagrams/nanvix.png
