a34ce3b30eecd306d3ec01d1b651d9b6b9f26fa3
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2018-07-23 19:25:03 -0300

    Updating Class Diagrams

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -6,6 +6,8 @@ $PLATUML doc/uml/kernel-hal.uml         && mv doc/uml/kernel-hal.png doc/class-d
 $PLATUML doc/uml/kernel-hal-mppa256.uml && mv doc/uml/kernel-hal-mppa256.png doc/class-diagrams/kernel-hal-mppa256.png
 $PLATUML doc/uml/kernel.uml             && mv doc/uml/kernel.png doc/class-diagrams/kernel.png
 $PLATUML doc/uml/libs-ipc-barrier.uml   && mv doc/uml/libs-ipc-barrier.png doc/class-diagrams/libs-ipc-barrier.png
-#$PLATUML doc/uml/libs-ipc.uml           && mv doc/uml/libs-ipc.png doc/class-diagrams/libs-ipc.png
-#$PLATUML doc/uml/nameserver.uml  && mv doc/uml/nameserver.png doc/class-diagrams/servers.name.png
-#$PLATUML doc/uml/nanvix.uml && mv doc/uml/nanvix.png doc/class-diagrams/nanvix.png
+$PLATUML doc/uml/libs-ipc-name.uml      && mv doc/uml/libs-ipc-name.png doc/class-diagrams/libs-ipc-name.png
+$PLATUML doc/uml/libs-ipc-mailbox.uml   && mv doc/uml/libs-ipc-mailbox.png doc/class-diagrams/libs-ipc-mailbox.png
+$PLATUML doc/uml/libs-ipc-portal.uml    && mv doc/uml/libs-ipc-portal.png doc/class-diagrams/libs-ipc-portal.png
+$PLATUML doc/uml/libs.uml               && mv doc/uml/libs.png doc/class-diagrams/libs.png
+$PLATUML doc/uml/nanvix.uml && mv doc/uml/nanvix.png doc/class-diagrams/nanvix.png
