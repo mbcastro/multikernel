65e3ed485a08e1fd5b08004eb387134d845b3fce
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2018-07-23 18:18:35 -0300

    Removing Semaphores Tests

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -46,5 +46,5 @@ run2 "nanvix-runtime-debug.img" "/test-driver" "/servers1" "--debug --portal"
 echo "=== Running Barrier Tests"
 run2 "nanvix-runtime-debug.img" "/test-driver" "/servers1" "--debug --barrier"
 
-echo "=== Running Semaphore Tests"
-run2 "nanvix-runtime-debug.img" "/test-driver" "/servers1" "--debug --semaphore"
+#echo "=== Running Semaphore Tests"
+#run2 "nanvix-runtime-debug.img" "/test-driver" "/servers1" "--debug --semaphore"
