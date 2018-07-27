81a5329b53e2c5f0a664018186e11962ec227e6e
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2018-07-27 11:33:19 -0300

    API Test: Named Semaphore Create Unlink CC

--- src/sbin/spawner0/test/master/semaphore/api.c
+++ src/sbin/spawner0/test/master/semaphore/api.c
@@ -28,8 +28,42 @@
 #include <mppaipc.h>
 
 #include <nanvix/limits.h>
+#include <nanvix/syscalls.h>
+#include <nanvix/pm.h>
 
 #include "test.h"
+
+/*============================================================================*
+ * Utilities                                                                  *
+ *============================================================================*/
+
+/**
+ * @brief PID of slaves.
+ */
+static int pids[NANVIX_PROC_MAX];
+
+/**
+ * @brief Spawn slave processes.
+ */
+static void spawn_slaves(const char **args)
+{
+	for (int i = 0; i < NANVIX_PROC_MAX; i++)
+		TEST_ASSERT((pids[i] = mppa_spawn(i, NULL, args[0], args, NULL)) != -1);
+}
+
+/**
+ * @brief Wait for slaves to terminate.
+ */
+static void join_slaves(void)
+{
+	int status;
+
+	for (int i = 0; i < NANVIX_PROC_MAX; i++)
+	{
+		TEST_ASSERT(mppa_waitpid(pids[i], &status, 0) != -1);
+		TEST_ASSERT(status == EXIT_SUCCESS);
+	}
+}
 
 /*==========================================================================*
  * API Test: Create Unlink                                                  *
@@ -68,13 +102,60 @@ static void test_posix_semaphore_open_close(void)
 	TEST_ASSERT(sem_unlink(semaphore_name) == 0);
 }
 
+/*============================================================================*
+ * API Test: Create Unlink CC                                                 *
+ *============================================================================*/
+
+/**
+ * @brief API Test: Create Unlink CC
+ */
+static void test_posix_semaphore_create_unlink_cc(void)
+{
+	int nodenum;
+	int barrier;
+	int nodes[NANVIX_PROC_MAX + 1];
+	char masternode_str[4];
+	char mailbox_nclusters_str[4];
+	char test_str[4];
+	const char *args[] = {
+		"/test/posix-semaphore-slave",
+		masternode_str,
+		mailbox_nclusters_str,
+		test_str,
+		NULL
+	};
+
+	nodenum = sys_get_node_num();
+
+	/* Build arguments. */
+	sprintf(masternode_str, "%d", nodenum);
+	sprintf(mailbox_nclusters_str, "%d", NANVIX_PROC_MAX);
+	sprintf(test_str, "%d", 0);
+
+	/* Build nodes list. */
+	nodes[0] = nodenum;
+	for (int i = 0; i < NANVIX_PROC_MAX; i++)
+		nodes[i + 1] = i;
+
+	/* Create barrier. */
+	TEST_ASSERT((barrier = barrier_create(nodes, NANVIX_PROC_MAX + 1)) >= 0);
+
+	spawn_slaves(args);
+
+	/* Wait for slaves. */
+	TEST_ASSERT(barrier_wait(barrier) == 0);
+
+	join_slaves();
+}
+
 /*============================================================================*/
 
 /**
  * @brief Unit tests.
  */
 struct test posix_semaphore_tests_api[] = {
-	{ test_posix_semaphore_create_unlink, "Create Unlink" },
-	{ test_posix_semaphore_open_close,    "Open Close"    },
-	{ NULL,                               NULL            },
+	{ test_posix_semaphore_create_unlink,    "Create Unlink" },
+	{ test_posix_semaphore_open_close,       "Open Close"    },
+	{ test_posix_semaphore_create_unlink_cc, "Create Unlink" },
+	{ NULL,                                  NULL            },
 };
