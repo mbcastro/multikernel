3c9f9618e7f24d16fc28d81cbee588f0447cf155
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2019-11-02 11:19:57 +0100

    Enhancement: Multithread Lib Runtime

--- src/libruntime/init.c
+++ src/libruntime/init.c
@@ -24,59 +24,66 @@
 
 #include <nanvix/runtime/runtime.h>
 #include <nanvix/runtime/stdikc.h>
+#include <nanvix/sys/thread.h>
 #include <nanvix/ulib.h>
 #include <posix/errno.h>
 
 /**
  * @brief Current runtime ring.
  */
-static int current_ring = -1;
+static int current_ring[THREAD_MAX] = {
+	[0 ... (THREAD_MAX - 1)] = -1
+};
 
 /**
  * @todo TODO: provide a detailed description for this function.
  */
 int __runtime_setup(int ring)
 {
+	int tid;
+
+	tid = kthread_self();
+
 	/* Invalid runtime ring. */
 	if (ring < 0)
 		return (-EINVAL);
 
 	/* Nothing to do. */
-	if (current_ring > ring)
+	if (current_ring[tid] > ring)
 		return (0);
 
 	/* Initialize unnamed IKC services. */
-	if ((current_ring < 0) && (ring >= 0))
+	if ((current_ring[tid] < 0) && (ring >= 0))
 	{
-		uprintf("[nanvix] initalizing ring 0");
+		uprintf("[nanvix][thread %d] initalizing ring 0", tid);
 		uassert(__stdsync_setup() == 0);
 		uassert(__stdmailbox_setup() == 0);
 		uassert(__stdportal_setup() == 0);
 	}
 
 	/* Initialize Name Service client. */
-	if ((current_ring < 1) && (ring >= 1))
+	if ((current_ring[tid] < 1) && (ring >= 1))
 	{
-		uprintf("[nanvix] initalizing ring 1");
+		uprintf("[nanvix][thread %d] initalizing ring 1", tid);
 		__name_setup();
 	}
 
 	/* Initialize Named IKC facilities. */
-	if ((current_ring < 2) && (ring >= 2))
+	if ((current_ring[tid] < 2) && (ring >= 2))
 	{
-		uprintf("[nanvix] initalizing ring 2");
+		uprintf("[nanvix][thread %d] initalizing ring 2", tid);
 		__nanvix_mailbox_setup();
 		__nanvix_portal_setup();
 	}
 
 	/* Initialize RMem Service client. */
-	if ((current_ring < 3) && (ring >= 3))
+	if ((current_ring[tid] < 3) && (ring >= 3))
 	{
-		uprintf("[nanvix] initalizing ring 3");
+		uprintf("[nanvix][thread %d] initalizing ring 3", tid);
 		__nanvix_rmem_setup();
 	}
 
-	current_ring = ring;
+	current_ring[tid] = ring;
 
 	return (0);
 }
@@ -86,26 +93,30 @@ int __runtime_setup(int ring)
  */
 int __runtime_cleanup(void)
 {
+	int tid;
+
+	tid = kthread_self();
+
 	/* Cleanup RMem Service client. */
-	if (current_ring >= 3)
+	if (current_ring[tid] >= 3)
 		__nanvix_rmem_cleanup();
 
 	/* Clean up IKC facilities. */
-	if (current_ring >= 2)
+	if (current_ring[tid] >= 2)
 	{
 		__nanvix_portal_cleanup();
 		__nanvix_mailbox_cleanup();
 	}
 
 	/* Clean up Name Service client. */
-	if (current_ring >= 1)
+	if (current_ring[tid] >= 1)
 		__name_cleanup();
 
 	uassert(__stdportal_cleanup() == 0);
 	uassert(__stdmailbox_cleanup() == 0);
 	uassert(__stdsync_cleanup() == 0);
 
-	current_ring = 0;
+	current_ring[tid] = 0;
 
 	return (0);
 }
