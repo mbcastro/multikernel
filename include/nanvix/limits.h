220d823daebb61cc5965fa2fdf9e10ce8061651e
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2020-03-19 16:46:56 -0300

    [ikc] Bug Fix: Missing Mailbox Message Size

--- include/nanvix/limits.h
+++ include/nanvix/limits.h
@@ -45,6 +45,11 @@
 	#define NANVIX_MAILBOX_MAX KMAILBOX_MAX
 
 	/**
+	 * @brief Maximum size for a mailbox message.
+	 */
+	#define NANVIX_MAILBOX_MESSAGE_SIZE KMAILBOX_MESSAGE_SIZE
+
+	/**
 	 * @brief Maximum number of portals that can be opened.
 	 */
 	#define NANVIX_PORTAL_MAX KPORTAL_MAX
